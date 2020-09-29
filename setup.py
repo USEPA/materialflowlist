ecefb581a23011f39908659876023cee3d74a199
Ben Young <44471635+bl-young@users.noreply.github.com>
2020-09-29 12:09:12 -0400

    Initial setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(
+    name='materialflowlist',
+    version='0.1',
+    packages=['materialflowlist'],
+    package_dir={'materialflowlist': 'materialflowlist'},
+    package_data={'materialflowlist': [
+        "input/*.*", "output/*.*", "flowmapping/*.*"]},
+    include_package_data=True,
+    install_requires = ['pandas>=0.22'],
+    url='https://github.com/USEPA/materialflowlist.git',
+    license='CC0',
+    author='Wesley Ingwersen',
+    author_email='ingwersen.wesley@epa.gov',
+    classifiers=[
+        "Development Status :: Alpha",
+        "Environment :: IDE",
+        "Intended Audience :: Science/Research",
+        "License :: CC0",
+        "Programming Language :: Python :: 3.x",
+        "Topic :: Utilities",
+    ],
+    description=''
+)
